0ead8720ab9229c048445a50f3d43adde627fdaf
rfpoulos <rfpoulos@outlook.com>
2018-03-02 13:31:43 -0500

    refactored code

--- scripts/main.js
+++ scripts/main.js
@@ -5,50 +5,8 @@ var coffeeOrder = document.querySelector("[name='coffee']");
 var emailAddress = document.querySelector("[name='emailAddress']");
 var flavor = document.querySelector("[name='flavor']");
 var strength = document.querySelector("[name='strength']");
-var allOrders = [];
+var localOrdersArray = [];
 
-
-var getOrders = function() {
-    $.get(URL, function(data) {
-        newArray = Object.values(data);
-        if (newArray !== null) {
-            newArray.forEach(function(item){
-                generateOrder(item);
-                allOrders.push(item);
-            });
-        };
-    });
-}
-
-var removeOrder = function(event) {
-    var myTarget = event.currentTarget;
-    myTarget.removeEventListener("click", removeOrder);
-    allOrders.forEach(function(item, index){
-        if (item._id === myTarget.getAttribute("data-thisID")) {
-            $.ajax({type:"DELETE",
-                 url:URL + "/" + item['emailAddress']});
-            allOrders.splice(index, 1);
-        };
-    });
-    var removeLi = function(){
-        orders.removeChild(myTarget);
-    }
-    setTimeout(removeLi, 2000);
-}
-var generateOrder = function(currentOrder) {
-    var order = document.createElement('li');
-    order.textContent = currentOrder['coffee'] + currentOrder['emailAddress'] + currentOrder['size'] + currentOrder['flavor'] + currentOrder['strength'] + " Remove: X";
-    order.setAttribute('data-thisID', currentOrder['_id']);
-    order.addEventListener("click", removeOrder);
-    orders.appendChild(order);
-}
-getOrders();
-var saveOrderToDataBase = function(currentOrder){
-    $.post(URL, currentOrder, function(data){
-        generateOrder(data);
-        allOrders.push(data);
-    });
-}
 var recordOrder = function(event) {
     event.preventDefault();
     var size = document.querySelector("[name='size']:checked");
@@ -61,4 +19,49 @@ var recordOrder = function(event) {
     saveOrderToDataBase(currentOrder);
 };
 
+var saveOrderToDataBase = function(currentOrder){
+    $.post(URL, currentOrder, function(data){
+        generateOrder(data);
+        localOrdersArray.push(data);
+    });
+}
+
+var generateOrder = function(currentOrder) {
+    var order = document.createElement('li');
+    order.textContent = currentOrder['coffee'] + currentOrder['emailAddress'] + currentOrder['size'] + currentOrder['flavor'] + currentOrder['strength'] + " Remove: X";
+    order.setAttribute('data-thisID', currentOrder['_id']);
+    order.addEventListener("click", removeOrder);
+    orders.appendChild(order);
+}
+var removeOrder = function(event) {
+    var myTarget = event.currentTarget;
+    myTarget.removeEventListener("click", removeOrder);
+    deleteItemFromServer(localOrdersArray, myTarget);
+    var removeRow = function(){
+        orders.removeChild(myTarget);
+    }
+    setTimeout(removeRow, 1000);
+}
+var deleteItemFromServer = function(anArray, element) {
+    anArray.forEach(function(item, index){
+        if (item._id === element.getAttribute("data-thisID")) {
+            $.ajax({type:"DELETE",
+                 url:URL + "/" + item['emailAddress']});
+            anArray.splice(index, 1);
+        };
+    });
+}
+var getOrders = function() {
+    $.get(URL, function(data) {
+        var newArray = Object.values(data);
+        localOrdersArray = newArray;
+        if (newArray !== null) {
+            newArray.forEach(function(item){
+                generateOrder(item);
+            });
+        };
+    });
+}
+
 coffeeForm.addEventListener("submit", recordOrder);
+getOrders();
